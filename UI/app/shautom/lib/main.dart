2cd3cb1f57bcb061752c01c2d3fcbc90f51a79b2
Ramsey Ith Njema <bsc-03-16@unima.ac.mw>
2022-02-04 00:25:24 +0200

    Created App HomePage

--- UI/app/shautom/lib/main.dart
+++ UI/app/shautom/lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:shautom/constants.dart';
-import 'package:shautom/views/home.dart';
+//import 'package:shautom/constants.dart';
+//import 'package:shautom/views/home.dart';
+import 'package:shautom/views/login.dart';
+import 'package:shautom/views/register.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+        fontFamily: 'Poppins',
+        primaryColor: Colors.white,
+        accentColor: Colors.blue),
+    home: HomePage(),
+  ));
 }
 
+/*
 class MyApp extends StatelessWidget {
   final String title = "Smart Home";
   // This widget is the root of your application.
@@ -18,7 +28,106 @@ class MyApp extends StatelessWidget {
         primaryColor: kPrimaryColor,
         scaffoldBackgroundColor: Colors.white,
       ),
-      home: HomeScreen(),
+      home: HomePage(),
     );
+  }
+}*/
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+        body: SafeArea(
+      child: Container(
+          width: double.infinity,
+          height: size.height,
+          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Text(
+                    "Welcome",
+                    style: TextStyle(
+                      color: Color(0xBD3F51B5),
+                      fontSize: 30,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    "SHAUTOM is an IoT-based smart home automation system that provides a platform for intelligent and efficient consumption of domestic utilities",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    height: size.height / 3,
+                    width: size.width * 0.6,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/logo.png'))),
+                  ),
+                  SizedBox(height: 60),
+                  Column(
+                    children: <Widget>[
+                      MaterialButton(
+                        color: Color(0xFF3F51B5),
+                        minWidth: double.infinity,
+                        height: 50,
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => LoginPage()));
+                        },
+                        shape: RoundedRectangleBorder(
+                            side: BorderSide(color: Colors.black),
+                            borderRadius: BorderRadius.circular(40)),
+                        child: Text("Login",
+                            style: TextStyle(
+                              fontFamily: 'Poppins',
+                              color: Colors.white,
+                              fontSize: 16,
+                            )),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      MaterialButton(
+                        color: Color(0xBD3F51B5),
+                        minWidth: double.infinity,
+                        height: 50,
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => RegistrationPage()));
+                        },
+                        shape: RoundedRectangleBorder(
+                            side: BorderSide(color: Colors.black),
+                            borderRadius: BorderRadius.circular(40)),
+                        child: Text("Register",
+                            style: TextStyle(
+                              fontFamily: 'Poppins',
+                              color: Colors.white,
+                              fontSize: 16,
+                            )),
+                      ),
+                    ],
+                  )
+                ],
+              )
+            ],
+          )),
+    ));
   }
 }
