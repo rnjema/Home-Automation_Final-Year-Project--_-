55e79d241174a8939b1c69e9c219c4f4634b0e25
Ramsey Ith Njema <bsc-03-16@unima.ac.mw>
2022-02-04 04:07:13 +0200

    Added bottom nav bar

--- UI/app/shautom/lib/views/login.dart
+++ UI/app/shautom/lib/views/login.dart
@@ -1,16 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg_provider/flutter_svg_provider.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: Container(),
       bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color(0xFF3F51B5),
+        iconSize: 30,
+        type: BottomNavigationBarType.fixed,
         items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
           BottomNavigationBarItem(
-              icon: Icon(Icons.verified_user), label: 'Profile'),
-          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings')
+              icon: ImageIcon(Svg('assets/images/icons/home.svg')),
+              label: 'Home'),
+          BottomNavigationBarItem(
+              icon: ImageIcon(
+                  Svg(
+                    'assets/images/icons/person.svg',
+                  ),
+                  size: 30),
+              label: 'Profile'),
+          BottomNavigationBarItem(
+              icon: ImageIcon(Svg('assets/images/icons/control.svg')),
+              label: "Control"),
+          BottomNavigationBarItem(
+              icon: ImageIcon(Svg('assets/images/icons/monitor.svg')),
+              label: 'Monitor'),
         ],
       ),
     );
