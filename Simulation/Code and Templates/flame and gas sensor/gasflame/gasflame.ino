253b8addc60d7b0e1e31450338256d4ae78166cc
Haddad-NYIRENDA <57199920+Haddad-NYIRENDA@users.noreply.github.com>
2022-02-07 13:14:21 +0200

    update

--- Simulation/Code and Templates/flame and gas sensor/gasflame/gasflame.ino
+++ Simulation/Code and Templates/flame and gas sensor/gasflame/gasflame.ino
@@ -2,17 +2,18 @@
 Servo myservo;
 int flame = 30;
 int smoke = 31;
-int red = 32;
-int green = 33 ;
+int green_LED = 32;
+int red_LED = 33;
 int servo = 34;
 int buzzer = 35 ;
+
 void setup ()
 {
-  pinMode (flame, INPUT) ;
-  pinMode (smoke, INPUT) ;
-  pinMode (red, OUTPUT) ;
-  pinMode (green, OUTPUT) ;
-  pinMode (buzzer, OUTPUT) ;
+  pinMode (flame, INPUT);
+  pinMode (smoke, INPUT);
+  pinMode (buzzer, OUTPUT);
+  pinMode(green_LED, OUTPUT);
+  pinMode(red_LED, OUTPUT);
   Serial.begin(9600);
   myservo.attach(servo);
   myservo.write(0);
@@ -22,25 +23,25 @@ void loop ()
 {
   int fval = digitalRead(flame);
   int sval = digitalRead(smoke);
-  Serial.print("fval = ");
+  Serial.print("flame = ");
   Serial.println(fval);
-  Serial.print("sval = ");
+  Serial.print("smoke = ");
   Serial.println(sval);
 
   if ( sval == HIGH or fval == HIGH)
   {
     Serial.println(" WARNING! ");
-    digitalWrite(red, HIGH);
+    digitalWrite(red_LED, HIGH);
+    digitalWrite(green_LED,LOW);
     digitalWrite(buzzer, HIGH);
-    digitalWrite(green, LOW);
     myservo.write(180);
   }
   else
   {
     Serial.println(" SAFE ");
-    digitalWrite(red, LOW);
-    digitalWrite(green, HIGH);
     digitalWrite(buzzer, LOW);
+    digitalWrite(red_LED, LOW);
+    digitalWrite(green_LED,HIGH);
     myservo.write(0);
   }
   delay(100);
