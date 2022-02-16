80e43ab49323f22614d9ee43d16fdf17ea105b18
Ramsey Ith Njema <bsc-03-16@unima.ac.mw>
2022-02-16 04:46:23 +0200

    Added monitoring template

---
+++ UI/app/shautom/lib/monitor.dart
@@ -0,0 +1,25 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:shautom/views/components/logo.dart';
+
+class MonitorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          //shape: ShapeBorder.lerp(a, b, t),
+          title: Text(
+            'miHome : Monitoring Dashboard',
+            style: TextStyle(),
+          ),
+          backgroundColor: Color(0xFF3F51B5),
+        ),
+        body: Column(
+          children: <Widget>[LogoWidget()],
+        ),
+      ),
+    );
+  }
+}
