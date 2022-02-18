7b14cbc5ad30a3f9ff0d42dd2420772aad17d0d1
Ramsey Ith Njema <bsc-03-16@unima.ac.mw>
2022-02-19 01:59:14 +0200

    Initialized stepper widget 4 registration

---
+++ UI/app/shautom/lib/views/registration_stepper.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:shautom/views/containers.dart';
+import 'package:shautom/views/form_fields.dart';
+import 'package:shautom/views/components/logo.dart';
+
+class RegistrationStepper extends StatelessWidget {
+  RegistrationStepper(Key? key, this.index, this.stepTapped, this.stepCancelled,
+      this.stepContinued)
+      : super(key: key);
+
+  final int index;
+  final Function(int) stepTapped;
+  final VoidCallback stepCancelled;
+  final VoidCallback stepContinued;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SafeArea(
+        child: Scaffold(
+            resizeToAvoidBottomInset: false,
+            body: Container(
+                height: size.height,
+                width: double.infinity,
+                child: Column(
+                    //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    //crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    //alignment: Alignment.center,
+                    children: <Widget>[
+                      Container(
+                          alignment: Alignment.topLeft,
+                          margin: EdgeInsets.only(top: 10, left: 10),
+                          child: GestureDetector(
+                            child: Row(children: [
+                              Container(
+                                padding: const EdgeInsets.only(right: 8.0),
+                                height: 16,
+                                child: Image(
+                                    image: AssetImage(
+                                        'assets/images/icons/back.png')),
+                              ),
+                              Text(
+                                "Back",
+                                style: TextStyle(
+                                    fontSize: 18, color: Color(0xFFC0B01D)),
+                              )
+                            ]),
+                            onTap: () => Navigator.pop(context),
+                          )),
+                      Stepper(
+                          currentStep: index,
+                          type: StepperType.vertical,
+                          onStepTapped: (val) => stepTapped(val),
+                          controlsBuilder: (BuildContext context,
+                                  {VoidCallback? onStepCancel,
+                                  VoidCallback? onStepContinue}) =>
+                              Row(
+                                children: <Widget>[
+                                  TextButton(
+                                      onPressed: onStepContinue,
+                                      child: Text('Next')),
+                                  TextButton(
+                                      onPressed: onStepCancel,
+                                      child: Text('Back'))
+                                ],
+                              ),
+                          steps: <Step>[
+                            Step(
+                                content: Text("This is it"),
+                                title: Text('Personal Details')),
+                            Step(
+                                content: Text("This is it again!"),
+                                title: Text('Contact Details'))
+                          ])
+                    ]))));
+  }
+}
+
+                
+                    
+                         
+      
+
+
+/*
+Column(
+                        children: [
+                          ListView.separated(
+                              //reverse: true,
+                              shrinkWrap: true,
+                              physics: ScrollPhysics(),
+                              scrollDirection: Axis.vertical,
+                              itemCount: registrationFields.length,
+                              separatorBuilder:
+                                  (BuildContext context, int index) => SizedBox(
+                                        height: size.height * 0.02,
+                                      ),
+                              itemBuilder: (BuildContext context, int index) {
+                                return TextFieldContainer(
+                                    child: TextFormField(
+                                  obscureText: registrationFields[index].hidden,
+                                  decoration: new InputDecoration(
+                                    border: InputBorder.none,
+                                    enabledBorder: InputBorder.none,
+                                    focusedBorder: InputBorder.none,
+                                    errorBorder: InputBorder.none,
+                                    //labelText: RegistrationFields[index].hintText,
+                                    hintText:
+                                        registrationFields[index].hintText,
+                                    icon: registrationFields[index].icon,
+                                  ),
+                                ));
+                              }),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                    padding: EdgeInsets.only(bottom: size.height * 0.01),
+                    width: size.width * 0.6,
+                    child: ElevatedButton(
+                      onPressed: () => print("Button pressed"),
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFF3F51B5),
+                        shape: new RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(40)),
+                      ),
+                      child: Text("Register",
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            color: Colors.white,
+                            fontSize: 16,
+                          )),
+                    )),
+              ]),
+
+ */
