13b6c3510dbffdef3f20822ad4a96bb4f2185bc4
Ramsey Ith Njema <bsc-03-16@unima.ac.mw>
2022-02-19 03:39:32 +0200

    Updated decoration and size attributes

--- UI/app/shautom/lib/views/containers.dart
+++ UI/app/shautom/lib/views/containers.dart
@@ -11,15 +11,15 @@ class TextFieldContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Container(
-      margin: EdgeInsets.symmetric(vertical: 3, horizontal: 2.5),
-      padding: EdgeInsets.symmetric(horizontal: 2, vertical: 2.5),
-      width: size.width * 0.4,
+      margin: EdgeInsets.symmetric(vertical: 3, horizontal: 0),
+      padding: EdgeInsets.symmetric(horizontal: 3, vertical: 2.5),
+      width: size.width * 0.6,
       height: size.height * 0.08,
       decoration: BoxDecoration(
           color: Colors.transparent,
           shape: BoxShape.rectangle,
           border: Border.all(color: Colors.black, width: 1),
-          borderRadius: BorderRadius.circular(40)),
+          borderRadius: BorderRadius.circular(20)),
       child: child,
     );
   }
