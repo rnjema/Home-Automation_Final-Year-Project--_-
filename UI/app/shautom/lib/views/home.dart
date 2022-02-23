e79df3dc60689ed4781acdbcb79ef7af936e2f6e
Ramsey Ith Njema <bsc-03-16@unima.ac.mw>
2022-02-23 11:37:32 +0200

    Homepage for authenticated users

--- UI/app/shautom/lib/views/home.dart
+++ UI/app/shautom/lib/views/home.dart
@@ -1,109 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:shautom/views/components/logo.dart';
-import 'package:shautom/views/register.dart';
-import 'package:shautom/views/login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_svg_provider/flutter_svg_provider.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
+    Size size = MediaQuery.of(context).size; //Media Device data config
+
+    final user = FirebaseAuth.instance.currentUser!;
+
     return Scaffold(
         //resizeToAvoidBottomInset: false,
         body: SafeArea(
-      child: Container(
-          width: double.infinity,
-          height: size.height,
-          padding: EdgeInsets.symmetric(
-              horizontal: 20, vertical: size.height * 0.01),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Column(
-                children: <Widget>[
-                  LogoWidget(),
-                  Text(
-                    "Welcome",
-                    style: TextStyle(
-                      color: Color(0xBD3F51B5),
-                      fontSize: 30,
-                      fontFamily: 'Poppins',
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(height: 4),
-                  RichText(
-                      text: TextSpan(
-                          children: <InlineSpan>[
-                            TextSpan(
-                                text: "miHome",
-                                style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                    color: Color(0xBD3F51B5))),
-                            TextSpan(
-                                text:
-                                    " is a companion app for SHAUTOM - an IoT-based smart home automation system that provides a platform for intelligent and efficient consumption of domestic utilities",
-                                style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.normal))
-                          ],
-                          style: TextStyle(
-                            color: Colors.grey,
-                          )),
-                      textAlign: TextAlign.center),
-                  SizedBox(height: size.height * 0.065),
-                  Column(
+            child: Container(
+                width: double.infinity,
+                height: size.height,
+                padding: EdgeInsets.symmetric(
+                    horizontal: 20, vertical: size.height * 0.01),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: <Widget>[
-                      MaterialButton(
-                        color: Color(0xFF3F51B5),
-                        minWidth: double.infinity,
-                        height: 50,
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => LoginPage()));
-                        },
-                        shape: RoundedRectangleBorder(
-                            side: BorderSide(color: Colors.black),
-                            borderRadius: BorderRadius.circular(40)),
-                        child: Text("Login",
-                            style: TextStyle(
-                              fontFamily: 'Poppins',
-                              color: Colors.white,
-                              fontSize: 16,
-                            )),
-                      ),
-                      SizedBox(
-                        height: size.height * 0.05,
-                      ),
-                      MaterialButton(
-                        color: Color(0xBD3F51B5),
-                        minWidth: double.infinity,
-                        height: 50,
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => RegistrationPage()));
-                        },
-                        shape: RoundedRectangleBorder(
-                            side: BorderSide(color: Colors.black),
-                            borderRadius: BorderRadius.circular(40)),
-                        child: Text("Register",
-                            style: TextStyle(
-                              fontFamily: 'Poppins',
-                              color: Colors.white,
-                              fontSize: 16,
-                            )),
-                      ),
-                    ],
-                  )
-                ],
-              )
-            ],
-          )),
-    ));
+                      Center(child: Text("Hello ${user.uid}"))
+                    ]))),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Color(0xFF3F51B5),
+          iconSize: 30,
+          type: BottomNavigationBarType.fixed,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: ImageIcon(Svg('assets/images/icons/home.svg')),
+                label: 'Home'),
+            BottomNavigationBarItem(
+                icon: ImageIcon(
+                    Svg(
+                      'assets/images/icons/person.svg',
+                    ),
+                    size: 30),
+                label: 'Profile'),
+            BottomNavigationBarItem(
+                icon: ImageIcon(Svg('assets/images/icons/control.svg')),
+                label: "Control"),
+            BottomNavigationBarItem(
+                icon: ImageIcon(Svg('assets/images/icons/monitor.svg')),
+                label: 'Monitor'),
+          ],
+        ));
   }
 }
