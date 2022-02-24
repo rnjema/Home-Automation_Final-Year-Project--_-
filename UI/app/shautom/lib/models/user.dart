0b9ee4e09e92a183d30dd9c884909c7af756a68e
Ramsey Ith Njema <bsc-03-16@unima.ac.mw>
2022-02-24 23:03:44 +0200

    Created sample User model

---
+++ UI/app/shautom/lib/models/user.dart
@@ -0,0 +1,14 @@
+class User {
+  final String? uid;
+  final String? firstName;
+  final String? lastName;
+  final String? emailAddress;
+  final String? phoneNumber;
+
+  User(
+      {this.uid,
+      this.firstName,
+      this.emailAddress,
+      this.lastName,
+      this.phoneNumber});
+}
