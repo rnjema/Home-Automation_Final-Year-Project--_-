798b1d3acb99b011da376d3adce5c3d8ec81d78a
Ramsey Ith Njema <bsc-03-16@unima.ac.mw>
2022-02-24 23:02:48 +0200

    Added control placeholder

---
+++ UI/app/shautom/lib/control.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:shautom/views/components/logo.dart';
+
+class ControlPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          appBar: AppBar(
+            //shape: ShapeBorder.lerp(a, b, t),
+            title: Text(
+              'Control',
+              style: TextStyle(),
+            ),
+            backgroundColor: Color(0xFF3F51B5),
+          ),
+          body: Container()),
+    );
+  }
+}
