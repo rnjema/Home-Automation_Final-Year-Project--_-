c061cd40a39955c450bb376fde263a91d04bc95d
Ramsey Ith Njema <bsc-03-16@unima.ac.mw>
2022-02-26 03:39:22 +0200

    Working on controls

--- UI/app/shautom/lib/control.dart
+++ UI/app/shautom/lib/control.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:shautom/views/components/logo.dart';
 
-class ControlPage extends StatelessWidget {
+class ControlPage extends StatefulWidget {
+  @override
+  State<ControlPage> createState() => _ControlPageState();
+}
+
+class _ControlPageState extends State<ControlPage> {
+  bool _isTvOn = false;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -10,11 +17,32 @@ class ControlPage extends StatelessWidget {
             //shape: ShapeBorder.lerp(a, b, t),
             title: Text(
               'Control',
-              style: TextStyle(),
+              style: TextStyle(color: Colors.blue),
             ),
-            backgroundColor: Color(0xFF3F51B5),
+            backgroundColor: Colors.grey.withAlpha(50),
+            elevation: 0, //Color(0xFF3F51B5)
           ),
-          body: Container()),
+          body: Container(
+            child: Column(children: [
+              SwitchListTile(
+                value: _isTvOn,
+                onChanged: (bool value) {},
+                title: Text("TV Appliance"),
+                //subtitle: Switch,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  side: BorderSide(style: BorderStyle.solid),
+                ),
+                tileColor: Colors.blueAccent,
+              ),
+              ListTile(
+                title: Text("Security Lights"),
+              ),
+              ListTile(
+                title: Text("Room 1"),
+              )
+            ]),
+          )),
     );
   }
 }
