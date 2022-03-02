8098d2f6aad6a275d1e7f586c61aecc722343751
Ramsey Ith Njema <bsc-03-16@unima.ac.mw>
2022-03-03 01:21:43 +0200

    Monitor dashboard taking shape

--- UI/app/shautom/lib/monitor.dart
+++ UI/app/shautom/lib/monitor.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:shautom/views/components/logo.dart';
+import 'package:shautom/views/components/temperature_widget.dart';
 
 class MonitorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return SafeArea(
-      child: Container(),
-    );
+        child: Container(
+            child: GridView(
+                shrinkWrap: true,
+                padding: EdgeInsets.only(left: 40, right: 40),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 15,
+                    crossAxisSpacing: 20),
+                physics: BouncingScrollPhysics(),
+                children: [
+          Container(child: TemperatureWidget(temperature: 25)),
+          Container(child: TemperatureWidget(temperature: 25)),
+          Container(child: TemperatureWidget(temperature: 25)),
+          Container(child: TemperatureWidget(temperature: 25)),
+        ])));
   }
 }
