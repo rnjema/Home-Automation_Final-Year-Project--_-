07d5bc68c4c8d60bd1c5443641b9c54ec0334ba1
Ramsey Ith Njema <bsc-03-16@unima.ac.mw>
2022-03-02 23:26:09 +0200

    Dummy

--- UI/app/shautom/lib/control.dart
+++ UI/app/shautom/lib/control.dart
@@ -11,26 +11,52 @@ class _ControlPageState extends State<ControlPage> {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return SafeArea(
-      child: Column(children: [
-        SwitchListTile(
-          value: _isTvOn,
-          onChanged: (bool value) {},
-          title: Text("TV Appliance"),
-          //subtitle: Switch,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20),
-            side: BorderSide(style: BorderStyle.solid),
-          ),
-          tileColor: Colors.blueAccent,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: GridView(
+          shrinkWrap: true,
+          physics: BouncingScrollPhysics(),
+          children: [
+            Container(
+              height: size.height * 0.1,
+              child: Column(children: [
+                Center(
+                  child: Icon(
+                    Icons.lightbulb,
+                    size: 50,
+                    color: Colors.blue,
+                  ),
+                )
+              ]),
+              decoration: BoxDecoration(
+                  color: Colors.grey.withAlpha(40),
+                  borderRadius: BorderRadius.circular(10)),
+            ),
+            Container(
+              height: size.height * 0.1,
+              decoration: BoxDecoration(
+                  color: Colors.grey.withAlpha(40),
+                  borderRadius: BorderRadius.circular(10)),
+            ),
+            Container(
+              height: size.height * 0.1,
+              decoration: BoxDecoration(
+                  color: Colors.grey.withAlpha(40),
+                  borderRadius: BorderRadius.circular(10)),
+            ),
+            Container(
+              height: size.height * 0.1,
+              decoration: BoxDecoration(
+                  color: Colors.grey.withAlpha(40),
+                  borderRadius: BorderRadius.circular(10)),
+            ),
+          ],
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, mainAxisSpacing: 10, crossAxisSpacing: 10),
         ),
-        ListTile(
-          title: Text("Security Lights"),
-        ),
-        ListTile(
-          title: Text("Room 1"),
-        )
-      ]),
+      ),
     );
   }
 }
