ed94a5ea85c47e193842df05e82a3566ae569014
Ramsey Ith Njema <bsc-03-16@unima.ac.mw>
2022-03-03 04:56:37 +0200

    Updated monitoring

--- UI/app/shautom/lib/monitor.dart
+++ UI/app/shautom/lib/monitor.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:shautom/views/components/temperature_widget.dart';
+import 'package:shautom/views/components/readings.dart';
+import 'package:firebase_database/firebase_database.dart';
 
-class MonitorPage extends StatelessWidget {
+class MonitorPage extends StatefulWidget {
+  @override
+  State<MonitorPage> createState() => _MonitorPageState();
+}
+
+class _MonitorPageState extends State<MonitorPage> {
+  final datasbaseRef = FirebaseDatabase.instance.ref();
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -17,9 +25,9 @@ class MonitorPage extends StatelessWidget {
                 physics: BouncingScrollPhysics(),
                 children: [
           Container(child: TemperatureWidget(temperature: 25)),
+          Container(child: HumidityWidget(humidity: 25)),
           Container(child: TemperatureWidget(temperature: 25)),
-          Container(child: TemperatureWidget(temperature: 25)),
-          Container(child: TemperatureWidget(temperature: 25)),
+          Container(child: HumidityWidget(humidity: 25)),
         ])));
   }
 }
