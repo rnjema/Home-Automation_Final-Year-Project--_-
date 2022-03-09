ff43a46fd009f4bd17af907b6a691fe5a865afef
Ramsey Ith Njema <bsc-03-16@unima.ac.mw>
2022-03-09 05:16:14 +0200

    Added sliverAppBar

--- UI/app/shautom/lib/control.dart
+++ UI/app/shautom/lib/control.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shautom/views/components/logo.dart';
 
 class ControlPage extends StatefulWidget {
   @override
@@ -7,54 +6,16 @@ class ControlPage extends StatefulWidget {
 }
 
 class _ControlPageState extends State<ControlPage> {
-  bool _isTvOn = false;
-
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return SafeArea(
-      child: Container(
-        padding: EdgeInsets.all(10),
-        child: GridView(
-          shrinkWrap: true,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2, mainAxisSpacing: 10, crossAxisSpacing: 10),
-          physics: BouncingScrollPhysics(),
-          children: [
-            Container(
-              height: size.height * 0.1,
-              child: Column(children: [
-                Icon(
-                  Icons.lightbulb,
-                  size: 50,
-                  color: Colors.blue,
-                )
-              ]),
-              decoration: BoxDecoration(
-                  color: Colors.grey.withAlpha(40),
-                  borderRadius: BorderRadius.circular(10)),
-            ),
-            Container(
-              height: size.height * 0.1,
-              decoration: BoxDecoration(
-                  color: Colors.grey.withAlpha(40),
-                  borderRadius: BorderRadius.circular(10)),
-            ),
-            Container(
-              height: size.height * 0.1,
-              decoration: BoxDecoration(
-                  color: Colors.grey.withAlpha(40),
-                  borderRadius: BorderRadius.circular(10)),
-            ),
-            Container(
-              height: size.height * 0.1,
-              decoration: BoxDecoration(
-                  color: Colors.grey.withAlpha(40),
-                  borderRadius: BorderRadius.circular(10)),
-            ),
-          ],
-        ),
-      ),
-    );
+        child: CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          title: Text("Control Dashboard"),
+        )
+      ],
+    ));
   }
 }
