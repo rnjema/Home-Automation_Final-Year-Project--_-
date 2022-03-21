009f7409583d18111ad9da83981dd21c9dec86fc
Ramsey Ith Njema <bsc-03-16@unima.ac.mw>
2022-03-21 13:45:11 +0200

    Added new list tabs

--- UI/app/shautom/lib/profile.dart
+++ UI/app/shautom/lib/profile.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg_provider/flutter_svg_provider.dart';
 import 'package:shautom/views/home.dart';
 
 import 'models/user.dart';
 
 class ProfilePage extends StatelessWidget {
   //final GlobalKey<_HomePageState> homeKey = GlobalKey<_HomePageState>();
-  UserModel? user;
+  final UserModel? user;
 
   ProfilePage({
     Key? key,
@@ -16,26 +17,96 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    return SafeArea(
-      child: Column(
-        children: [
-          CircleAvatar(
-              minRadius: size.width * 0.15,
-              child: Text(
-                user == null
-                    ? "AZ"
-                    : "${user?.firstName![0].toUpperCase()}${user?.lastName![0].toUpperCase()}",
-                style: TextStyle(fontSize: 60),
-              )),
-          Center(
-              child: Column(
-            children: [
-              Text("${user!.firstName} ${user!.lastName}"),
-              Text("${user!.emailAddress}"),
-            ],
-          ))
-        ],
-      ),
+    return SafeArea(child: Settings(user: user));
+  }
+}
+
+class Settings extends StatelessWidget {
+  final UserModel? user;
+  Settings({Key? key, required this.user}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return _mySettings(
+      context,
     );
+  }
+
+  Widget _mySettings(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+        child: ListView(
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: 20),
+          decoration: BoxDecoration(
+              color: Colors.transparent,
+              borderRadius: BorderRadius.circular(10)),
+          height: size.height * 0.2,
+          width: double.infinity,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(children: [
+                CircleAvatar(
+                    backgroundColor: Colors.blue.withOpacity(0.6),
+                    radius: size.width * 0.15,
+                    child: Text(
+                      user == null
+                          ? "AZ"
+                          : "${user?.firstName![0].toUpperCase()}${user?.lastName![0].toUpperCase()}",
+                      style: TextStyle(fontSize: size.width * 0.12),
+                    )),
+              ]),
+              Padding(
+                padding: const EdgeInsets.only(top: 40, left: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("${user!.firstName} ${user!.lastName}"),
+                    SizedBox(height: 10),
+                    Text("${user!.emailAddress}"),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+        ListTile(
+          leading: Icon(
+            Icons.person,
+            color: Colors.blue.withOpacity(0.6),
+          ),
+          title: Text("Profile"),
+          subtitle: Text("User Details"),
+          trailing: Container(child: Icon(Icons.arrow_forward_ios)),
+        ),
+        ListTile(
+          leading: Icon(
+            Icons.settings,
+            color: Colors.blue.withOpacity(0.6),
+          ),
+          title: Text("Settings"),
+          subtitle: Text("System Configuration"),
+          trailing: Container(child: Icon(Icons.arrow_forward_ios)),
+        ),
+        ListTile(
+          leading: Icon(
+            Icons.data_object,
+            color: Colors.blue.withOpacity(0.6),
+          ),
+          title: Text("Data"),
+          subtitle: Text("Data Configuration"),
+          trailing: Container(child: Icon(Icons.arrow_forward_ios)),
+        ),
+        ListTile(
+          leading: Icon(Icons.info, color: Colors.blue.withOpacity(0.6)),
+          minVerticalPadding: 3,
+          title: Text("About"),
+          subtitle: Text("System Details"),
+          trailing: Container(child: Icon(Icons.arrow_forward_ios)),
+        )
+      ],
+    ));
   }
 }
