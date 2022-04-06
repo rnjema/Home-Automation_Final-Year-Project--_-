ba6f52dd14f1bcf689cd566b4999ebff4477c4c7
Ramsey Ith Njema <bsc-03-16@unima.ac.mw>
2022-04-06 11:14:36 +0200

    Moved chart state up nearest parent

--- UI/app/shautom/lib/views/components/graph.dart
+++ UI/app/shautom/lib/views/components/graph.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:math' as math;
+
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:intl/intl.dart';
 
@@ -41,9 +44,17 @@ class _PowerGraphState extends State<PowerGraph> {
             xAxisName: "Time",
             yAxisName: "Energy Value")
       ],
-      primaryXAxis: NumericAxis(edgeLabelPlacement: EdgeLabelPlacement.shift),
+      primaryXAxis: NumericAxis(
+          edgeLabelPlacement: EdgeLabelPlacement.shift,
+          majorGridLines: MajorGridLines(width: 0),
+          title: AxisTitle(
+              text: "Timestamp",
+              textStyle: TextStyle(fontSize: 10),
+              alignment: ChartAlignment.center)),
       primaryYAxis: NumericAxis(
+          majorGridLines: MajorGridLines(width: 0),
           labelFormat: '{value}',
+          title: AxisTitle(text: "Energy"),
           numberFormat: NumberFormat.decimalPattern("en-us")),
     );
   }
@@ -62,6 +73,78 @@ class _PowerGraphState extends State<PowerGraph> {
 
 class EnergyData {
   EnergyData({
+    required this.timestamp,
+    required this.value,
+  });
+
+  final double timestamp;
+  final double value;
+}
+
+class LivePowerGraph extends StatelessWidget {
+  final TooltipBehavior? tooltipBehaviour;
+  ChartSeriesController? chartSeriesController;
+  List<LiveEnergyData> chartData;
+
+  LivePowerGraph({
+    Key? key,
+    this.tooltipBehaviour,
+    this.chartSeriesController,
+    required this.chartData,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SfCartesianChart(
+      //title: ChartTitle(text: "Energy Consumption Anlaysis"),
+      legend: Legend(isVisible: false),
+      tooltipBehavior: tooltipBehaviour,
+      series: <SplineAreaSeries>[
+        SplineAreaSeries<LiveEnergyData, double>(
+            animationDuration: 2000,
+            onRendererCreated: (ChartSeriesController controller) {
+              chartSeriesController = controller;
+            },
+            name: 'Energy Consumption',
+            dataSource: chartData,
+            xValueMapper: (LiveEnergyData energy, _) => energy.timestamp,
+            yValueMapper: (LiveEnergyData energy, _) => energy.value,
+            dataLabelSettings: DataLabelSettings(isVisible: false),
+            enableTooltip: true,
+            color: Colors.blue,
+            opacity: 0.6,
+            splineType: SplineType.cardinal,
+            cardinalSplineTension: 0.7,
+            xAxisName: "Time",
+            yAxisName: "Energy Value")
+      ],
+      primaryXAxis: NumericAxis(
+          edgeLabelPlacement: EdgeLabelPlacement.shift,
+          title: AxisTitle(
+              text: "Timestamp",
+              textStyle: TextStyle(fontSize: 10),
+              alignment: ChartAlignment.center)),
+      primaryYAxis: NumericAxis(
+          labelFormat: '{value}',
+          title: AxisTitle(text: "Energy"),
+          numberFormat: NumberFormat.decimalPattern("en-us")),
+    );
+  }
+
+  double time = 5;
+  void updateDataSource(Timer timer) {
+    chartData.add(LiveEnergyData(
+        timestamp: time++, value: ((math.Random().nextDouble()) * 400)));
+    chartData.removeAt(0);
+    chartSeriesController?.updateDataSource(
+      addedDataIndex: chartData.length - 1,
+      removedDataIndex: 0,
+    );
+  }
+}
+
+class LiveEnergyData {
+  LiveEnergyData({
     required this.timestamp,
     required this.value,
   });
